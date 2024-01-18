34d720ead2ffad2dedafea46add27f340888377d
Yorgos Chatziparaskevas <georgio13@users.noreply.github.com>
2024-01-18 17:06:34 +0200

    Update validator

--- src/app/trips-page/services/booking.service.ts
+++ src/app/trips-page/services/booking.service.ts
@@ -1,20 +1,17 @@
 import {DatabaseService} from '../../shared/services/database.service';
 import {Injectable} from '@angular/core';
-import {UserService} from '../../shared/services/user.service';
 
 @Injectable()
 export class BookingService {
   private readonly serviceModel: string;
 
-  constructor(private databaseService: DatabaseService,
-              private userService: UserService) {
+  constructor(private databaseService: DatabaseService) {
     this.serviceModel = 'booking';
   }
 
   public async insertBooking(booking: any): Promise<any> {
     try {
-      const user = await this.userService.getMe();
-      const url = this.databaseService.formatURL(booking, `${this.serviceModel}/${user.afm}`);
+      const url = this.databaseService.formatURL(booking, this.serviceModel);
       return await this.databaseService.postRequest({}, url);
     } catch (error) {
       throw error;
